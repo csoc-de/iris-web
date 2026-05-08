948ab5c08fc0b0aa35a5115cf0afeed2729b3888
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2026-05-08 10:42:31 +0200

    [FIX] Keep clicked `alert_tags` filter on refresh by properly filling the form input

--- source/app/static/assets/js/iris/alerts.js
+++ source/app/static/assets/js/iris/alerts.js
@@ -786,29 +786,8 @@ function generateDefinitionList(obj) {
 }
 
 function addTagFilter(this_object) {
-    let tag_name = $(this_object).data('tag');
-    let filters = getFiltersFromUrl();
-
-    if (filters['alert_tags']) {
-        for (let tag of filters['alert_tags'].split(',')) {
-            if (tag === tag_name) {
-                return;
-            }
-        }
-        filters['alert_tags'] += `,${tag_name}`;
-    } else {
-        filters['alert_tags'] = tag_name;
-    }
-
-    const queryParams = new URLSearchParams(window.location.search);
-    let page_number = parseInt(queryParams.get('page'));
-    let per_page = parseInt(queryParams.get('per_page'));
-
-    updateAlerts(page_number, per_page, filters)
-        .then(() => {
-            notify_success('Refreshed');
-            $('#newAlertsBadge').text(0).hide();
-        });
+    $('#alert_tags').val($(this_object).data('tag'))
+    refreshAlerts();
 }
 
 function getFiltersFromUrl() {
