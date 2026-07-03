f5c4166957a48ee0039b6c05bbc620e6ffdfc0ad
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2026-07-03 13:07:11 +0200

    [FIX] Show "Alerts are being deleted" message only if user confirmed the prompt

--- source/app/static/assets/js/iris/alerts.js
+++ source/app/static/assets/js/iris/alerts.js
@@ -2133,14 +2133,14 @@ async function deleteBatchAlerts(data_content= {}) {
 
     do_deletion_prompt(`You are about to delete ${selectedAlerts.length} alerts`, true)
     .then((doDelete) => {
-       window.swal({
-              title: "Alerts are being deleted, please wait",
-              text: "This window will close automatically when it's done",
-              icon: "/static/assets/img/loader.gif",
-              button: false,
-              closeOnClickOutside: false
-        });
         if (doDelete) {
+            window.swal({
+                   title: "Alerts are being deleted, please wait",
+                   text: "This window will close automatically when it's done",
+                   icon: "/static/assets/img/loader.gif",
+                   button: false,
+                   closeOnClickOutside: false
+             });
             const data = {
                 'alert_ids': selectedAlerts,
                 'csrf_token': $('#csrf_token').val()
